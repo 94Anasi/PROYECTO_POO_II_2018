a0d91e3f0ce3995142d91e87c13f0f868dc81719
LAB4-PC7 <LAB4-PC7@LAB4-PC>
2018-10-26 11:48:38 -0500

    Se declararon las clases o TDA bases para Amazon Viewer

---
+++ src/com/aanasi/amazonviewer/vista/AplicacionMain.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.aanasi.amazonviewer.vista;
+
+/**
+ *
+ * @author LAB4-PC7
+ */
+public class AplicacionMain {
+    public static void main(String[] args) {
+        System.out.println("HOLA MUNDO");
+    }
+    
+}
